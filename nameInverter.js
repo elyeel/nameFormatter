8938af5627733e3dc5a4b92b43fb327af0986dfe
Risyardi Iswanto <elyeel@yahoo.com>
2019-08-04 05:21:52 +0000

    function and test are in working order

--- nameInverter.js
+++ nameInverter.js
@@ -1,5 +1,42 @@
 const nameInverter = function(name) {
-
+  let result = [];
+  let tempName = "";
+  if (name === undefined) { //throws error for 'undefined'
+    return "Error";
+  };
+  
+  if (name === "") {
+    return tempName;
+  }
+  tempName = (name.trim()).split(' ');
+  // const lastIndex = tempName[0].length -1;
+  // if (tempName[lastIndex] === '.') {
+  //   console.log(lastIndex);
+  //   return '';
+  // }
+  
+  switch (tempName.length) {
+    case 1:
+      if (tempName[0].indexOf('.') > 0) {
+        return '';
+      } else {
+        return tempName[0];
+      }
+      break;
+    case 2:
+      if (tempName[0].indexOf('.') > 0) {
+        return tempName;
+      } else {
+        return tempName.reverse();
+      }
+      break;
+    case 3:
+      result.push(tempName[0]);
+      result.push(tempName[2]);
+      result.push(tempName[1]);
+      break;
+  }
+  return result;
 }
 
 module.exports = nameInverter;
--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -5,4 +5,57 @@ const nameInverter = require('../nameInverter');
 
 describe('nameInverter', function() {
 
+  it("return an empty string when passed an empty string", function() { // 1
+    const input = "";
+    const output = nameInverter(input);
+    assert.equal(input,output);
+  });
+
+  it("return a single name when passed a single name", function() { // 2
+    // const input = "name";
+    const output = nameInverter("name");
+    assert.equal("name",output);
+  });
+
+  it("return a single name when passed a single name with extra spaces", function() { // 3
+    const output = nameInverter(" name ");
+    assert.equal("name",output);
+  });
+
+  it("return a last-name, first-name when passed a first and last-name", function() { // 4
+    const output = nameInverter("first last");
+    assert.deepEqual(['last', 'first'],output);
+  });
+
+  it("return a last-name, first-name when passed a first and last-name with extra spaces around the names", function() { // 5
+    const output = nameInverter(" first last");
+    assert.deepEqual(["last", "first"],output);
+  });
+
+  it("return an empty string when passed a single honorific", function() { // 6
+    const output = nameInverter("Dr. ");
+    assert.equal('',output);
+  });
+
+  it("return honorific first-name when passed honorific first-name", function() { // 7
+    const output = nameInverter("Dr. first");
+    assert.deepEqual(['Dr.', 'first'],output);
+  });
+
+  it("return a honorific last-name, first-name when passed honorific first-name last-name", function() { // 8
+    const output = nameInverter("Dr. first-name last-name");
+    assert.deepEqual(["Dr.", "last-name", "first-name"],output);
+  });
+
+  it("return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words", function() { // 9
+    const output = nameInverter(" Dr. first-name last-name ");
+    assert.deepEqual(["Dr.", "last-name", "first-name"],output);
+  });
+
+  it("throw an error when name is undefined", function() { // 10
+    const input = undefined;
+    const output = nameInverter(input);
+    assert.equal("Error",output);
+  });
+
 });
